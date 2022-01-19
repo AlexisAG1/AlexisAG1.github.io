89d6f8cf35bcf3b8e8de59897964e157f739a8ea
AlexisAG1 <alexis2000agp@gmail.com>
2022-01-19 13:30:09 -0600

    ps

---
+++ funciones.i.js
@@ -0,0 +1,13 @@
+function ir(){
+    
+var c=0000;
+var u="APLANET"; 
+   if(document.form.password.value==c && document.form.login.value==u){
+   alert ("BIENVENIDO ");
+   window.location="index.html";}
+
+   else{
+   alert("Intente de nuevo");
+}
+
+}
