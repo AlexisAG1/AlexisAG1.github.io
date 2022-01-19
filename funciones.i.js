7e77a98a04546eda59408b1a7b4b4c0292d9b273
AlexisAG1 <alexis2000agp@gmail.com>
2022-01-19 14:01:07 -0600

    cs

--- funciones.i.js
+++ funciones.i.js
@@ -4,7 +4,7 @@ var c=0000;
 var u="APLANET"; 
    if(document.form.password.value==c && document.form.login.value==u){
    alert ("BIENVENIDO ");
-   window.location="index.html";}
+   window.location="Inicio.html";}
 
    else{
    alert("Intente de nuevo");
